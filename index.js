30e72216b84b1acb9579b086a911c5d89e96e9aa
CMSOTO2 <81086284+CMSOTO2@users.noreply.github.com>
2021-11-13 21:05:50 -0500

    Synced Carousel
    Synced Carousel Section added

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ $(".slider").slick({
   slidesToScroll: 1,
   arrows: true,
 });
+
+//Synced carousel
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+});
