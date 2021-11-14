98f2e88a67e6f3a199fd2a5487e1c2d5becf3d1c
CMSOTO2 <81086284+CMSOTO2@users.noreply.github.com>
2021-11-13 21:51:16 -0500

    Footer + slider responsiveness
    Added footer and slider responsiveness

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ $(".slider").slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   arrows: true,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1090,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 //Synced carousel
@@ -19,4 +51,36 @@ $(".slider-nav").slick({
   slidesToScroll: 1,
   asNavFor: ".slider-for",
   dots: false,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1090,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
