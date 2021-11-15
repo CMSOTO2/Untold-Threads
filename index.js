5a54dec046bfa7cdac4bf42b8eb6e1754b91fcf0
CMSOTO2 <81086284+CMSOTO2@users.noreply.github.com>
2021-11-14 23:03:13 -0500

    Artist We Work With page added
    artist slider added

--- index.js
+++ index.js
@@ -84,3 +84,42 @@ $(".slider-nav").slick({
     },
   ],
 });
+//artist slider
+// $(".artist-slider").slick({
+//   infinite: true,
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+//   arrows: true,
+//   responsive: [
+//     {
+//       breakpoint: 1400,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         infinite: true,
+//       },
+//     },
+//     {
+//       breakpoint: 1090,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         infinite: true,
+//       },
+//     },
+//     {
+//       breakpoint: 800,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 570,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
